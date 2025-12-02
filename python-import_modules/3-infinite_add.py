702909fbb88ac07e00ccdc0e5301c979e0846e72
Fidan <fbbagirova2006@gmail.com>
2025-12-02 13:01:54 +0000

    3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import sys
+
 s = 0
 if __name__ == "__main__":
-    for i in range(1,len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         s = s + int(sys.argv[i])
     print(s)
-        
-
-            
