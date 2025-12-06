3ecddcf8f5cbaacbdf88219365a1b58821670787
unknown <fbbagirova2006@gmail.com>
2025-12-07 01:11:19 +0400

    hello

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    result = None
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-    retun result
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]), end='')
+            counter += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return counter
