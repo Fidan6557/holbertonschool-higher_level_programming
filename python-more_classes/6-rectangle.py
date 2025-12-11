5c545e10ffd8a0e1465c7b6592078a0ae0c33c91
unknown <fbbagirova2006@gmail.com>
2025-12-11 20:32:15 +0400

    hello

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,6 +4,8 @@
 
 class Rectangle:
     """Rectangle class with private width and height attributes"""
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
