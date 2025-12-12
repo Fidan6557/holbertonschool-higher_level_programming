1014fd69eb997db30b3cacda24f96918d46e3380
unknown <fbbagirova2006@gmail.com>
2025-12-12 17:51:14 +0400

    hello

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+My module document
+'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    My method document
+    '''
+    return issubclass(type(obj), a_class) and not type(obj) is a_class
