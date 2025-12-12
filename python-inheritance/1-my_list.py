5037c24aff71a26e8e38433587e08d88e910a6eb
unknown <fbbagirova2006@gmail.com>
2025-12-12 15:48:23 +0400

    hello

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-'''
+"""
 My module document
-'''
+"""
 
 
 class MyList(list):
-    '''
+    """
     My class document
-    '''
+    """
     def print_sorted(self):
+        """Prints the list in ascending sorted order"""
         print(sorted(self))
