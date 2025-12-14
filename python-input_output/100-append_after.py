ea4fed77000fb6454209793a4a3d18a012f7e34f
unknown <fbbagirova2006@gmail.com>
2025-12-14 16:35:13 +0400

    hello

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a function that inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ A function that inserts a line of text to a file"""
+    with open(filename, "a") as f:
+        f.write(new_string)
