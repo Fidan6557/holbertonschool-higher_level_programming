e933de28034da29e81c48c0a9819a58aab15652c
unknown <fbbagirova2006@gmail.com>
2025-12-14 16:04:45 +0400

    hello

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A class Student that defines a student"""
+
+
+class Student:
+    """A class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    filtered[attr] = self.__dict__[attr]
+            return filtered
+
+    def reload_from_json(self, json):
+        """Function that replaces all attributes"""
+        for key, value in json.items():
+            setattr(self, key, value)
