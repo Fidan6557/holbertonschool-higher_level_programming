de0757799fbe953165adb08afcb3193ce3f59619
unknown <fbbagirova2006@gmail.com>
2025-12-14 16:52:12 +0400

    hello

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""a function that inserts a line of text to a file"""
-
-
 def append_after(filename="", search_string="", new_string=""):
-    """ A function that inserts a line of text to a file"""
-    with open(filename, "r+w") as f:
-        f.write(new_string)
+    """Inserts new_string after each line containing search_string"""
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    with open(filename, "w") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
