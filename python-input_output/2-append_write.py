c8b6262933fd6a5d912d15163fc87caf432a7b73
unknown <fbbagirova2006@gmail.com>
2025-12-14 13:31:25 +0400

    hello

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Appends a string at the end of a text file (UTF8) and returns the number of"""
+""" Appends a string at the end of a text file (UTF8)"""
 
 
 def append_write(filename="", text=""):
-    """ Appends a string at the end of a text file (UTF8) and returns the number """
+    """ Appends a string at the end of a text file (UTF8) """
     with open(filename, "a") as f:
         return f.write(text)
