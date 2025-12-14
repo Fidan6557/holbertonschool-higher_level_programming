e7feb21d87faf61b782bfc52e146c4a9630cbb4b
unknown <fbbagirova2006@gmail.com>
2025-12-14 16:23:56 +0400

    hello

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    p= []
+    for i in range(n):
+        row =[1]
+        for j in range(1,i):
+            row.append(p[i-1][j-1]+p[i-1][j])
+        if i>0:
+            row.append(1)
+        p.append(row)
+    return p
