58792068d1d6e3e4c37d6d717a23c1cc09b51478
Fidan Bagirova <fidan@example.com>
2025-12-16 16:02:56 +0000

    HU

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,5 +1,6 @@
 --  a script that displays the number of records with id = 89 in the table first_table
 --  a script that displays the number of records with id = 89 in the table first_table
-SELECT COUNT (*)
+SELECT 
+COUNT (*)
 FROM first_table
 WHERE ID = 89;
