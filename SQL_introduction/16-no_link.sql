5b4029b1615653957363b0806cadb0d6095493fc
Fidan Bagirova <fidan@example.com>
2025-12-16 16:46:28 +0000

    SD

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,7 +1,7 @@
 -- a script that lists all records of the table second_table of the database
 -- a script that lists all records of the table second_table of the database
 SELECT
-SCORE AS 'score'
+SCORE AS 'score',
 NAME AS 'name'
 FROM second_table
 WHERE NAME IS NOT NULL
