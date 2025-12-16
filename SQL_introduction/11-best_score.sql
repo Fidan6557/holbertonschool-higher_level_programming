e2e17c4b8405786ef18a2e8e5f78d0c863a0ab32
Fidan Bagirova <fidan@example.com>
2025-12-16 16:25:35 +0000

    dchnc

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,6 @@
 -- a script that lists all records with a score >= 10
 -- in the table second_table of the database hbtn_0c_0 in server
-SELECT score, name  WHERE score >= 10 FROM second_table
+SELECT score, name  
+WHERE score >= 10 
+FROM second_table
 ORDER BY score DESC limit = 10;
