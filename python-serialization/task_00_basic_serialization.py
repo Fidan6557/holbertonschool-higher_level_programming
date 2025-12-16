7bad49c973d91a7b1ba886a977d0d93c4485febe
Your Name <you@example.com>
2025-12-16 10:51:42 +0000

    1

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" basic serialization module that adds the functionality
+to serialize a Python dictionary to a JSON file and deserialize
+ the JSON file to recreate the Python Dictionary"""
+import  json
+
+
+def serialize_and_save_to_file(data, filename):
+    def serialize(obj):
+        if isinstance(obj, set):
+            return {
+                "__type__": "set",
+                "elements": list(obj)
+            }
+        raise TypeError("Object type not serializable")
+
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, default=serialize)
+
+
+def load_and_deserialize(filename):
+    def deserialize(obj):
+        if "__type__" in obj and obj["__type__"] == "set":
+            return set(obj["elements"])
+        return obj
+
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file, object_hook=deserialize)
