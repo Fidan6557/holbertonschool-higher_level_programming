2167b2811c8ffd5616c9508a6f636639f19aa3ba
Fidan Bagirova <fidan@gmail.com>
2025-12-19 12:25:53 +0000

    g

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database hbtn_0d_usa and the table states
 -- a script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
