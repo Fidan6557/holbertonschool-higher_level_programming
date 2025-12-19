f4b135f1fee43bd48ad8f4e4a6118a1e75938a5a
Fidan Bagirova <fidan@gmail.com>
2025-12-19 12:19:39 +0000

    H

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- a script that creates the table unique_id
 -- a script that creates the table unique_id
-CREATE TABLE IF NOT EXISTS unique_id (id INT NOT NULL, 
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1, 
                                     name VARCHAR(256));
