b799f2b2f03a863c2fd2fca7e5fc53ed5c18b53d
Fidan Bagirova <fidan@gmail.com>
2025-12-21 14:11:07 +0000

    S

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,14 +1,10 @@
 -- creates the database hbtn_0d_usa and the table cities
---creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
 );
